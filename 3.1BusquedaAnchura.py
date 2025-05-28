c8b43d70169eb2995a7a5eb444fb5cf8692645c8
Julio Cesar Cazarez Uriarte <julcaz191087@outlook.es>
2025-05-27 23:03:27 -0700

    Corregido

--- 3.1BusquedaAnchura.py
+++ 3.1BusquedaAnchura.py
@@ -1,50 +1,7 @@
-from collections import deque
-
-GOAL_STATE = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-MOVES = {
-    0: [1, 3],
-    1: [0, 2, 4],
-    2: [1, 5],
-    3: [0, 4, 6],
-    4: [1, 3, 5, 7],
-    5: [2, 4, 8],
-    6: [3, 7],
-    7: [4, 6, 8],
-    8: [5, 7]
-}
-
-def get_neighbors(state):
-    neighbors = []
-    zero_pos = state.index(0)
-    for move in MOVES[zero_pos]:
-        new_state = state[:]
-        new_state[zero_pos], new_state[move] = new_state[move], new_state[zero_pos]
-        neighbors.append(new_state)
-    return neighbors
-
-def is_solvable(state):
-    inv_count = 0
-    for i in range(8):
-        for j in range(i+1, 9):
-            if state[i] and state[j] and state[i] > state[j]:
-                inv_count += 1
-    return inv_count % 2 == 0
-
-def print_path(came_from, end):
-    path = []
-    while end:
-        path.append(end)
-        end = came_from[tuple(end)]
-    path.reverse()
-    for step in path:
-        print_board(step)
-        print()
-
-def print_board(state):
-    for i in range(0, 9, 3):
-        print(state[i], state[i+1], state[i+2])
+from utils_8puzzle import *
 
 def breadth_first_search(start):
+    from collections import deque
     visited = set()
     queue = deque([start])
     came_from = {tuple(start): None}
